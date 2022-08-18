5f03341f0d2895337ba1ce578e09737e9da63a50
Hadi Tabe <tabealhoje.hadi@gmail.com>
2022-08-18 18:54:41 +0430

    Update solve.js
    persian

--- src/controls/solve.js
+++ src/controls/solve.js
@@ -9,7 +9,7 @@ var solveControl = L.Control.extend({
     // Add reference to map.
     map.solveControl = this;
     this._div = L.DomUtil.create('div', 'custom-control icon-control solve-control');
-    this._div.title = 'Solve';
+    this._div.title = 'اجرا';
     this._div.onclick = function(e) {
       L.DomEvent.stopPropagation(e);
       map.removeControl(solveControl);
